512cce97e9ec5d934884b1b375ef04aed5782555
Gyeong <chung3286@naver.com>
2020-04-20 19:07:19 +0900

    JAVA file

---
+++ GetPi/src/GetPi.java
@@ -0,0 +1,14 @@
+public class GetPi {
+	public static void main(String[] args) {
+		int howmany = 2000000;
+		double mark=1, denominator=1, pi=0;
+		
+		for(int i=0; i<howmany; i++) {
+			pi = pi + mark * (1/denominator);
+			mark *= -1;
+			denominator += 2;
+		}
+		pi *= 4;
+		System.out.println(pi);
+	}
+}
